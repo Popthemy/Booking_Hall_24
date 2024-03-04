99cb401b7a9f7e73f600ff22ad024ffb815a0b4a
Popoola Temilorun <141974860+Popthemy@users.noreply.github.com>
2024-03-04 02:56:23 +0100

    set debug to false settings.py

--- Book_hall/settings.py
+++ Book_hall/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@=tt4_$^!g2lz288$j_+!5ug87%nctd%+ff=pxi7hxtb-1y^+g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS =    [ '0.0.0.0:5578' ,'localhost', '127.0.0.1', 'localhost:800'] 
+ALLOWED_HOSTS =    [ "*"] 
 
 
 # Application definition
