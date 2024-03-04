7251fdf00f5485e1bcfcb2ab58fe6acf8c5521d8
Popoola Temilorun <141974860+Popthemy@users.noreply.github.com>
2024-03-04 13:10:49 +0100

    Update settings.py

--- Book_hall/settings.py
+++ Book_hall/settings.py
@@ -55,11 +55,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
 
     'whitenoise.middleware.WhiteNoiseMiddleware', #for static files when Debug=False
 
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
